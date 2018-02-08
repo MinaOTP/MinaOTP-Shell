06786d93af3bf360c813225f1e314ee93296f65e
lancegin <gin.lance.inside@hotmail.com>
2018-02-08 11:12:10 +0800

    upd: standard the argparse module

--- mina/mina.py
+++ mina/mina.py
@@ -5,38 +5,78 @@
 # @Link    : 
 # @Disc    : add remove show and list totp tokens in the terminal
 
+import logging
 import argparse
+
+# configure the basic logging level
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s] %(name)s:%(levelname)s: %(message)s"
+)
     
 # the main function to control the script
 def main():
+    # Define the basic_parser and subparsers
+    logging.info('Initial basic_parser')
+
     _desc = 'MinaOTP is a two-factor authentication tool that runs in the terminal'
-    parser = argparse.ArgumentParser(description=_desc)
-
-    parser.add_argument(
-        dest='list',
-        action='store',
-        help='list all tokens that have been added to local'
-    )
-    parser.add_argument(
-        '-a',
-        dest='new_token',
-        action='store',
-        help='add a new token'
-    )
-    parser.add_argument(
-        '-r',
-        dest='err_token',
-        action='store',
-        help='remove a token'
-    )
-    parser.add_argument(
-        '-s',
-        dest='token_to_show',
-        action='store',
-        help='show a token on-time'
+    basic_parser = argparse.ArgumentParser(description=_desc)
+    subparsers = basic_parser.add_subparsers(
+        dest="command",
+        help="Available commands"
     )
 
-    args = parser.parse_args()
+    # Subparser for the list command
+    logging.debug("Initial list subparser")
+
+    list_parser = subparsers.add_parser(
+        "list",
+        help="List all tokens."
+    )
+
+    # Subparser for the add command
+    logging.debug("Initial add subparser")
+
+    add_parser = subparsers.add_parser(
+        "add",
+        help="Add a new token."
+    )
+
+    # Subparser for the remove command
+    logging.debug("Initial remove subparser")
+
+    remove_parser = subparsers.add_parser(
+        "remove",
+        help="Remove a token."
+    )
+
+    # Subparser for the show command
+    logging.debug("Initial show subparser")
+
+    show_parser = subparsers.add_parser(
+        "show",
+        help="Show a token on-time"
+    )
+    show_parser.add_argument(
+        "token",
+        help="issuer of the token"
+    )
+
+    # handle the args input by user
+    args = basic_parser.parse_args()
+    # convert arguments to dict
+    arguments = vars(args)
+    command = arguments.pop("command")
+
+    if command == "list":
+        print("list all tokens")
+    if command == "add":
+        print("add a new token")
+    if command == "remove":
+        print("remove a token")
+    if command == "show":
+        print("show a token on-time")
+
 
 if __name__ == '__main__':
     main()
