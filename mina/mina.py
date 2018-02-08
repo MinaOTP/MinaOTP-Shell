b79d06010c18c09bbf15a6a991ebef6e778c38fe
lancegin <gin.lance.inside@hotmail.com>
2018-02-08 14:16:13 +0800

    done: list all tokens

--- mina/mina.py
+++ mina/mina.py
@@ -8,27 +8,42 @@
 import json
 import logging
 import argparse
+import pyotp
+
+# global variable
+ISSUER_LEN = 16
+REMARK_LEN = 16
+OTP_LEN = 16
 
 # configure the basic logging level
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="[%(asctime)s] %(name)s:%(levelname)s: %(message)s"
 )
+
 # load tokens from json file
 def load_json(json_url):
-    with open(json_url, 'r') as f:
+    with open(json_url, "r") as f:
         return json.load(f)
 
 # list all tokens
-def list():
-    logging.info("List all tokens")
+def list(tokens):
+    print("SECRET".center(ISSUER_LEN, "="), "REMARK".center(REMARK_LEN, "="), "OTP".center(OTP_LEN, "="))
+    for token in tokens:
+        secret = token["secret"]
+        issuer = token["issuer"]
+        remark = token["remark"]
+        # generate tmp TOTO object and calculate the token
+        totp_tmp = pyotp.TOTP(secret)
+        current_otp = totp_tmp.now()
+        print(issuer.center(ISSUER_LEN), remark.center(REMARK_LEN), current_otp.center(OTP_LEN))
 
 # the main function to control the script
 def main():
     # Load the json file
     _json_url = './.mina.json'
     tokens = load_json(_json_url)
-    print(tokens)
+
     # Define the basic_parser and subparsers
     logging.info('Initial basic_parser')
 
@@ -82,7 +97,7 @@ def main():
     command = arguments.pop("command")
 
     if command == "list":
-        list()
+        list(tokens)
     if command == "add":
         print("add a new token")
     if command == "remove":
