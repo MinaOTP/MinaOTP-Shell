3c37978178aecc850259bfb90a1ff2f72d19f68e
lancegin <gin.lance.inside@hotmail.com>
2018-02-09 16:39:04 +0800

    fix: import error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 import os
-import os.path
+from os import path
 
 here = path.abspath(path.dirname(__file__))
 
