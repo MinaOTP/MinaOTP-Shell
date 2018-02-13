13c24b3bfbac2ac4d988fdbcbbaeff599b284d01
Gin <gin.lance.inside@hotmail.com>
2018-02-13 15:07:59 +0800

    add: new version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.2',
+    version='1.0.3',
 
     description='TOTP authenticator implement as a terminal tool',
     long_description='TOTP authenticator implement as a terminal tool, and this project is developed in Python',
